1204c89090895cda2db6a350aeb3621cb632ee87
Alora Riley <alora.riley@gmail.com>
2025-03-11 19:38:02 -0500

    Fix: Readd 'type: :model' to user_spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe User do
+RSpec.describe User, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
